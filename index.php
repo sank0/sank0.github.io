b5dd469243c50b9ce8e97a511aa0ba8e38f12bcb
sank0 <jbesanko@gmail.com>
2020-08-26 06:04:53 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>GETTING STARTED WITH BRACKETS</title>
+        <meta name="description" content="sanko's page on github.">
+        <link rel="stylesheet" href="sanko.css">
+    </head>
+    <body>
+        
+        <h1>welcome to my site</h1>
+        <h2>enjoy your stay!</h2>
+        
+        <p>
+            Welcome to <a href="/">sank0.github.io</a>
+        </p>
+        
+        <p>
+            <? 
+                date_default_timezone_set("America/New_York");
+                echo "The time is " . date("h:i:sa");
+            ?>
+        </p>
+        
+<pre>
+
+    [[[[[[[[[[[[[[[      ]]]]]]]]]]]]]]]
+    [::::::::::::::      ::::::::::::::]
+    [::::::::::::::      ::::::::::::::]
+    [::::::[[[[[[[:      :]]]]]]]::::::]
+    [:::::[                      ]:::::]
+    [:::::[                      ]:::::]
+    [:::::[                      ]:::::]
+    [:::::[                      ]:::::]
+    [:::::[        sank0         ]:::::]
+    [:::::[                      ]:::::]
+    [:::::[                      ]:::::]
+    [:::::[                      ]:::::]
+    [:::::[                      ]:::::]
+    [:::::[                      ]:::::]
+    [::::::[[[[[[[:      :]]]]]]]::::::]
+    [::::::::::::::      ::::::::::::::]
+    [::::::::::::::      ::::::::::::::]
+    [[[[[[[[[[[[[[[      ]]]]]]]]]]]]]]]
+
+</pre>
+        
+    </body>
+</html>
+
